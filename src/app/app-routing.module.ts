b41fa8cadc821675004483f83547211d00dd5b48
jorgekik37@gmail.com <jorgekik37@gmail.com>
2023-07-27 13:48:12 -0600

    ft 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
+import { AgregarComponent } from './utl/agregar/agregar.component';
+import { AlumnosComponent } from './utl/alumnos/alumnos.component';
+import { EditarComponent } from './utl/editar/editar.component';
+import { LoginComponent } from './home/login/login.component';
+import { InicioComponent } from './home/inicio/inicio.component';
+import { MenuComponent } from './menu_admin/menu.component';
+import { ConocenosComponent } from './home/conocenos/conocenos.component';
+import { InventarioComponent } from './home/inventario/inventario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: InicioComponent, pathMatch:'full'},
+  {path:'inicio',component: InicioComponent},
+  {path:'conocenos',component: ConocenosComponent},
+  {path:'menu',component: MenuComponent},
+  {path:'inventario',component: InventarioComponent},
+
+  {path:'Agregar', component: AgregarComponent},
+  {path: 'verAlumnos',component: AlumnosComponent },
+  {path: 'Editar/:id',component: EditarComponent },
+  {path: 'login',component: LoginComponent },
+
+ 
+];;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+  
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlumnosComponent } from './utl/alumnos/alumnos.component';
+import { MenuComponent } from './menu_admin/menu.component';
+import { HomeComponent } from './home/home.component';
+import { AgregarComponent } from './utl/agregar/agregar.component';
+import { EditarComponent } from './utl/editar/editar.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material/material.module';
+import { AlumnoFilterPipe } from './utl/alumnos-filter.pipe';
+import { LoginComponent } from './home/login/login.component';
+import { InicioComponent } from './home/inicio/inicio.component';
+import { ConocenosComponent } from './home/conocenos/conocenos.component';
+import { InventarioComponent } from './home/inventario/inventario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent,
+    MenuComponent,
+    AlumnoFilterPipe,
+    HomeComponent,
+    AgregarComponent,
+    EditarComponent,
+    LoginComponent,
+    InicioComponent,
+    ConocenosComponent,
+    InventarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
